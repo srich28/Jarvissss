fb95d21e5a57f5b0f88eacfe78daf0ccf246f587
srich28 <silviarichter089@gmail.com>
2024-03-22 15:45:28 -0300

    day22

---
+++ main.py
@@ -0,0 +1,9 @@
+import os
+import eel
+
+eel.init("www")
+
+
+os.system('start msedge.exe --app="https://localhost:8000/index.html"')
+
+eel.start('index.html', mode=None, host='localhost', port=8000)
